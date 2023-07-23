2333c654eb76ec9c6d0e4475216861d571f3c007
Elo Colburn <elocolburn@comcast.net>
2023-07-22 18:40:47 -0700

    feat: socials in footer

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    export let onclick: (e:any) => void;
+    export let onclick: (e: any) => void;
 </script>
 
 <button on:click={onclick} class="bg-black border-purple-500 border-4 p-10 pl-20 pr-20 m-5">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import Button from "$lib/components/Button.svelte";
+	import { redirect } from "@sveltejs/kit";
 </script>
 
 <div class="grid grid-cols-1 grid-rows-1 place-items-center">
     <h1 class="text-3xl">Radix Project Dashboard</h1>
-    <Button onclick={() => { console.log("test"); }}>
+    <Button onclick={() => { console.log("redirect"); redirect(307, "/new_project") }}>
         New Project
     </Button>
 </div>
